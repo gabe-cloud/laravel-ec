b68ab827021a4f0d4599839436751e74dee6cf99
Gabriel Ramos <109767094+gabe-cloud@users.noreply.github.com>
2025-12-04 01:14:21 -0400

    style: styling for user home page

---
+++ resources/js/pages/user/Index.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <UserLayouts>
+        <div class="bg-white">
+            <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <h2 class="text-2xl font-bold tracking-tight text-gray-900">Latest Product List</h2>
+
+                <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                    <div v-for="product in products" :key="product.id" class="group relative">
+                        <img v-if="product.product_images.length > 0" :src="`/${product.product_images[0].image}`"
+                            :alt="product.imageAlt"
+                            class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80" />
+                        <img v-else src="https://static.vecteezy.com/system/resources/previews/022/059/000/non_2x/no-image-available-icon-vector.jpg"
+                            :alt="product.imageAlt"
+                            class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80" />
+                        <div class="mt-4 flex justify-between">
+                            <div>
+                                <h3 class="text-sm text-gray-700">
+                                    <a :href="product.href">
+                                        <span aria-hidden="true" class="absolute inset-0"></span>
+                                        {{ product.title }}
+                                    </a>
+                                </h3>
+                                <p class="mt-1 text-sm text-gray-500">{{ product.brand.name }}</p>
+                            </div>
+                            <p class="text-sm font-medium text-gray-900">${{ product.price }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </UserLayouts>
+
+</template>
+
+<script setup>
+
+import UserLayouts from './Layouts/UserLayouts.vue'
+
+defineProps({
+    products: Array
+})
+
+</script>
---
+++ resources/js/pages/user/Layouts/UserLayouts.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- header -->
+    <Header />
+    <!-- end header -->
+
+    <!-- add  -->
+    <Hero></Hero>
+    <!-- end  -->
+
+    <!-- main content -->
+    <slot />
+    <!-- end main content  -->
+
+    <!-- footer -->
+    <Footer></Footer>
+    <!-- end footer  -->
+</template>
+
+<script setup>
+
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import Hero from './Hero.vue'
+
+onMounted(() => {
+    initFlowbite();
+})
+
+</script>
